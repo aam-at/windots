2f80c6429f9ca36d412cf5b88c11145c57e71940
Alexander Matyasko <alexander.matyasko@gmail.com>
2025-10-23 03:32:07 +0800

    Wezterm: install wezterm

--- scripts/Setup.ps1
+++ scripts/Setup.ps1
@@ -328,6 +328,7 @@ function Ensure-Link($dest, $src) {
 # -----------------------
 $linkMap = @{
     ($PROFILE.CurrentUserAllHosts) = (RepoPath 'scripts\Profile.ps1')
+    (Join-Path $HOME '.config\wezterm') = (Join-Path $HOME 'dotfiles\config\wezterm')
     (Join-Path $HOME '.config\yasb') = (RepoPath 'yasb')
     (Join-Path $HOME '.gitconfig') = (RepoPath 'git\config')
     (Join-Path $HOME '.ideavimrc') = (Join-Path $HOME 'dotfiles\idea\ideavimrc')
